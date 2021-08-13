8b2badc66bdb5949bd422a0603788f5f603b1d98
imogen <imogen@hyraxbio.com>
2021-08-13 16:32:32 +0100

    initial commit

---
+++ nanohiv.py
@@ -0,0 +1,151 @@
+import click
+
+import os
+import sys
+import tempfile
+
+# run minimap2 with nanopore settings
+def map(ref, fq, gap_open, f_out):
+
+    with open(f_out, 'w') as out:
+        subprocess.call(["minimap2",
+                        "-x",
+                        "map-ont",
+                        "-O", gap_open,
+                        "-E", gap_open/2,
+                        "-a",
+                        fq], 
+                        stdout=out
+    )
+
+# take a minimap2 samfile, compress, sort and index it
+def prepare_bam(sam, bam):
+    
+    with open(bam, 'w') as out:
+
+        # compress
+        temp1, temp_name1 = tempfile.mkstemp()
+        subprocess.call(["samtools",
+                        "view",
+                        "-b",
+                        sam],
+                        stdout=temp1
+        )
+        os.close(temp1)
+
+        # sort
+        temp2, temp_name2 = tempfile.mkstemp()
+        subprocess.call(["samtools",
+                        "sort",
+                        temp_name],
+                        stdout=temp2
+        )
+        os.close(temp2)
+
+        # index
+        subprocess.call(["samtools",
+                        "index",
+                        temp_name2],
+                        stdout=bam
+        )
+
+        os.unlink(temp_name1)
+        os.unlink(temp_name2)
+
+# generate consensus sequence from bam file,
+# reference and fastq
+def generate_consensus(ref, fq, bam, fasta):
+
+    temp, temp_name = tempfile.mkstep()
+    subprocess.call(["nanopolish",
+                    "variants",
+                    "--max-haplotypes",
+                    "3000", 
+                    "--faster",
+                    "--fix-homopolymers",
+                    "--snps",
+                    "-t", 
+                    "32",
+                    "-p",
+                    "1",
+                    "--reads", fq,
+                    "--bam", bam,
+                    "--genome", ref],
+                    stdout=temp
+    )
+    os.close(temp)
+    subprocess.call(["nanopolish",
+                     "vcf2fasta",
+                     "-g", ref,
+                     temp_name],
+                     fasta
+    )
+    os.unlink(temp_name)
+
+@click.option(
+    '--reference',
+     type=click.Path(exists=True, readable=True, resolve_path=True)
+)
+@click.option(
+    '--reads',
+     type=click.Path(exists=True, readable=True, resolve_path=True)
+)
+@click.option(
+    '--output',
+     type=click.Path(resolve_path=True)
+)
+@click.option(
+    '--standard-gap-penalty', default=4
+)
+@click.option(
+    '--lower-gap-penalty', default=1
+)
+
+def nanohiv(reference, reads, output, standard_gap_penalty, lower_gap_penalty):
+    """
+    Check consensus sequences for intactness.
+    """
+
+    if reference is None:
+        raise ValueError('Need a --reference.')
+    
+    if reads is None:
+        raise ValueError('Need a fastq file --reads.')
+    
+    if output is None:
+        raise ValueError('Need an --output file.')
+
+    sam, sam_name = tempfile.mkstemp()
+    bam, bam_name = tempfile.mkstemp()
+    vcf, vcf_name = tempfile.mkstemp()
+
+    # generate a consensus
+    print("Generating initial consensus.")
+    consensus1, consensus1_name = tempfile.mkstemp()
+    map(reference, reads, standard_gap_penalty, sam_name)
+    prepare_bam(sam_name, bam_name)
+    generate_consensus(reference, reads, bam_name, consensus_1)
+
+    # remap to consensus with lower gap penalty
+    print("Remapping reads to consensus with lower gap penalty.")
+    consensus2, consensus2_name = tempfile.mkstemp()
+    map(consensus1_name, reads, lower_gap_penalty, sam_name)
+    prepare_bam(sam_name, bam_name)
+    generate_consensus(consensus1_name, reads, bam_name, consensus_2)
+
+    # final remapping to consensus
+    print("Final remapping to consensus.")
+    map(consensus2_name, reads, standard_gap_penalty, sam_name)
+    prepare_bam(sam_name, bam_name)
+    generate_consensus(consensus2_name, reads, bam_name, output)
+
+    os.unlink(sam_name)
+    os.unlink(bam_name)
+    os.unlink(vcf_name)
+    os.unlink(consensus1_name)
+    os.unlink(consensus2_name)
+
+    print("Complete.")
+
+if __name__ == '__main__':
+   nanohiv()
